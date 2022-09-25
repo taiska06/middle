16592067909f38985ce1200328aa4481bf9997a9
taiska06 <112109525+taiska06@users.noreply.github.com>
2022-09-25 14:40:35 +0300

    Delete class.h

--- class/class.h
+++
@@ -1,8 +0,0 @@
-#ifndef CLASS_H_INCLUDED
-#define CLASS_H_INCLUDED
-#include <iostream>
-void itc_num_print(int number);
-int itc_sum_num(long long number);
-int itc_len_num(long long number);
-int itc_multi_num(long long number);
-#endif // CLASS_H_INCLUDED
