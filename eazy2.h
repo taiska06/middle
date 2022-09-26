96e267b148581c3fcd716478f32603b4dfa291ab
taiska06 <112109525+taiska06@users.noreply.github.com>
2022-09-26 13:53:13 +0300

    Delete eazy2.h

--- eazy2.h
+++
@@ -1,9 +0,0 @@
-#ifndef EAZY2_H_INCLUDED
-#define EAZY2_H_INCLUDED
-#include <iostream>
-int itc_max(int a,int b);
-int itc_min(int a, int b);
-double itc_fmax(double a, double b);
-double itc_fmin(double a, double b);
-int itc_sqrt(int num);
-#endif // EAZY2_H_INCLUDED
