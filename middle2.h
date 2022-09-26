4e3c74ddb35af3efaa9448444a7a6a17261493e6
taiska06 <112109525+taiska06@users.noreply.github.com>
2022-09-26 14:52:15 +0300

    Delete middle2.h

--- middle2.h
+++
@@ -1,8 +0,0 @@
-#ifndef MIDDLE2_H_INCLUDED
-#define MIDDLE2_H_INCLUDED
-#include <iostream>
-int itc_max_num(long long number);
-int itc_min_num(long long number);
-int itc_rev_num(long long number);
-
-#endif // MIDDLE2_H_INCLUDED
