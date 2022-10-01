fad01dd350a4681a8e622d701510ae3b83988044
taiska06 <112109525+taiska06@users.noreply.github.com>
2022-10-01 15:44:04 +0300

    Add files via upload

---
+++ middle000.cpp
@@ -0,0 +1,59 @@
+
+
+#include "middle.h"
+
+long long itc_bin_num(long long number) {
+    int a,b;
+     a = 0;
+     b = 1;
+    while (number > 0) {
+         a = a + b * (number % 2);
+         b=b* 10;
+         number = number / 2;
+    }
+    return a;
+}
+
+long long itc_oct_num(long long number){
+
+ int a,b;
+     a = 0;
+     b = 1;
+    while (number > 0) {
+         a = a + b * (number % 8);
+         b=b* 10;
+         number = number / 8;
+    }
+    return a;
+
+}
+int itc_rev_bin_num(long long number) {
+     int a,b,c;
+     a = 0;
+     b = 1;
+     c=0;
+    while (number > 0) {
+        c =number % 10;
+        if (c >= 2)
+            return -1;
+        a = a+(c * b);
+        b = b*2;
+        number = number/10;
+    }
+    return a;
+}
+int itc_rev_oct_num(long long number) {
+     int a,b,c;
+     a = 0;
+     b = 1;
+     c=0;
+    while (number > 0) {
+        c =number % 10;
+        if (c >= 8)
+            return -1;
+        a = a+(c * b);
+        b = b*8;
+        number = number/10;
+    }
+    return a;
+}
