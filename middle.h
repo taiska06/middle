df0389a7398903af707e96b15baf51a60110beaf
taiska06 <112109525+taiska06@users.noreply.github.com>
2022-10-01 15:42:16 +0300

    Add files via upload

--- middle.h
+++ middle.h
@@ -1,13 +1,8 @@
 #ifndef MIDDLE_H_INCLUDED
 #define MIDDLE_H_INCLUDED
 #include <iostream>
-void itc_num_print(int number);
-int itc_len_num(long long number);
-int itc_sum_num(long long number);
-int itc_multi_num(long long number);
-int itc_null_count(long long number);
-int itc_max_num(long long number);
-int itc_min_num(long long number);
-int itc_rev_num(long long number);
-
+long long itc_bin_num(long long number);
+long long itc_oct_num(long long number);
+int itc_rev_bin_num(long long number);
+int itc_rev_oct_num(long long number);
 #endif // MIDDLE_H_INCLUDED
