53d3f608f1b35860d94a37324e41f383f8774537
taiska06 <112109525+taiska06@users.noreply.github.com>
2022-10-01 18:51:52 +0300

    Update middle.h

--- middle.h
+++ middle.h
@@ -10,6 +10,7 @@ int itc_max_num(long long number);
 int itc_min_num(long long number);
 int itc_rev_num(long long number);
 bool itc_mirror_num(long long number);
+int itc_second_max_num(long long number);
 long long itc_bin_num(long long number);
 long long itc_oct_num(long long number);
 int itc_rev_bin_num(long long number);
