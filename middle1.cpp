ab2bf730e9f151227d3cc5ec19680f877eb7f20c
taiska06 <112109525+taiska06@users.noreply.github.com>
2022-10-02 15:00:10 +0300

    Update middle1.cpp

--- middle1.cpp
+++ middle1.cpp
@@ -2,7 +2,6 @@
 
 #include "middle.h"
 using namespace std;
-
  void itc_num_print(int number){
     cout << number<<endl;
 
@@ -10,6 +9,9 @@ using namespace std;
 int itc_len_num(long long number){
      int d;
      d=0;
+     if (number<0){
+        number=number*-1;
+     }
  while (number > 0){
     number=number/10;
     d=d+1;
@@ -20,6 +22,9 @@ int itc_sum_num(long long number){
      int i,sum;
       i=0;
       sum=0;
+       if (number<0){
+        number=number*-1;
+     }
  while (number > 0){
     i=number%10;
     sum=sum+i;
@@ -32,18 +37,24 @@ int itc_sum_num(long long number){
      int i,sum;
       i=0;
       sum=1;
+        if (number<0){
+        number=number*-1;
+     }
  while (number > 0){
     i=number%10;
     sum=sum*i;
     number=number/10;
 
  }
- return sum;
+return sum;
  }
  int itc_null_count(long long number){
  int kol,c;
  kol=0;
- while(number > 0){
+ if (number<0){
+        number=number*-1;
+     }
+ while(number != 0){
     c=number%10;
     if(c==0){
         kol=kol+1;
