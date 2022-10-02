faa17bffd85bcdb4a98d7a995306a11889d05701
taiska06 <112109525+taiska06@users.noreply.github.com>
2022-10-02 14:19:24 +0300

    Add files via upload

---
+++ middle.cpp
@@ -0,0 +1,23 @@
+#include "middle.h"
+
+
+
+int itc_second_simple_max_num(long long number) {
+    if (number / 10 == 0)
+        return -1;
+    int max,b,i;
+   max = itc_max_num(number);
+    b = -1;
+    while (number > 0) {
+        i =number % 10;
+         if (i == max)
+            return -1;
+        if (i > b && i != max)
+            b = i;
+        number = number / 10;
+    }
+    return b;
+}
+
+
+
