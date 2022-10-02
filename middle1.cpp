578d1314f2cb56ca19ec24f57882012b8d8b3201
taiska06 <112109525+taiska06@users.noreply.github.com>
2022-10-02 14:25:06 +0300

    Update middle1.cpp

--- middle1.cpp
+++ middle1.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int itc_len_num(long long number){
      int d;
      d=0;
- while (number != 0){
+ while (number > 0){
     number=number/10;
     d=d+1;
 }
@@ -20,7 +20,7 @@ int itc_sum_num(long long number){
      int i,sum;
       i=0;
       sum=0;
- while (number != 0){
+ while (number > 0){
     i=number%10;
     sum=sum+i;
     number=number/10;
@@ -43,7 +43,7 @@ int itc_sum_num(long long number){
  int itc_null_count(long long number){
  int kol,c;
  kol=0;
- while(number != 0){
+ while(number > 0){
     c=number%10;
     if(c==0){
         kol=kol+1;
