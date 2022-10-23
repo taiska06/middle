f3b98093c2f1cc2c2ef559c9bf932f82238d9cb5
taiska06 <112109525+taiska06@users.noreply.github.com>
2022-10-23 10:40:21 +0300

    Update middle.cpp

--- middle.cpp
+++ middle.cpp
@@ -8,6 +8,9 @@ int itc_second_simple_max_num(long long number) {
     int max,b,i;
    max = itc_max_num(number);
     b = -1;
+     if (number<0){
+        number=number*-1;
+     }
     while (number > 0) {
         i =number % 10;
          if (i == max)
