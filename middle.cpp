21fa09c6ecff2f103c6bc6530fd9096aa4dd8354
taiska06 <112109525+taiska06@users.noreply.github.com>
2022-11-20 21:33:54 +0300

    Update middle.cpp

--- middle.cpp
+++ middle.cpp
@@ -1,26 +1,36 @@
 #include "middle.h"
 using namespace std;
 
-
 int itc_second_simple_max_num(long long number) {
-    if (number / 10 == 0)
+          if (number / 10 == 0)
         return -1;
-    int max,b,i;
+    int max,b,i,c;
+    c=0;
    max = itc_max_num(number);
     b = -1;
+     if (number/10==0){
+        return -1;
+     }
      if (number<0){
         number=number*-1;
      }
     while (number > 0) {
         i =number % 10;
-         if (i == max)
-            return -1;
+        if (i == max){
+           c++;
+        }
         if (i > b && i != max)
             b = i;
         number = number / 10;
-    }
-    return b;
-}
 
+    }
+    if(c>1){
+        return -1;
+    }
+    else{
+        return b;
+    }
+
+}
 
 
